b7d42675b9f801c3a80c12196b938e52b1a99d8c
InsaneLoafer <Insane_Loafer@163.com>
2020-11-07 22:23:13 +0800

    练习

---
+++ practice/appium_pra/appium_p.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time     : 2020/11/7 11:59
+# @Author   : ZhangTao
+# @File     : appium_p.py
+
+from appium import webdriver
+desired_caps = {}
+desired_caps['platformName'] = 'Android'
