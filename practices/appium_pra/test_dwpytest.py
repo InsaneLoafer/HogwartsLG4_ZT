854168a47e3ccf1507378a0a58a6fc8fff8bddef
InsaneLoafer <Insane_Loafer@163.com>
2020-11-10 18:07:21 +0800

    练习

--- practices/appium_pra/test_dwpytest.py
+++ practices/appium_pra/test_dwpytest.py
@@ -25,6 +25,7 @@ class TestDW():
     def teardown(self):
         self.driver.quit()
 
+    @pytest.mark.skip
     def test_search(self):
         print('搜索测试用例')
         """
@@ -39,6 +40,33 @@ class TestDW():
         self.driver.find_element_by_xpath('//*[@resource-id="com.xueqiu.android:id/name" and @text="阿里巴巴"]').click()
         current_price = self.driver.find_element_by_id('com.xueqiu.android:id/current_price').text
         assert current_price > 200
+
+    def test_search1(self):
+        """
+        1、打开雪球  应用首页
+        2、定位首页的搜索框
+        3、判断搜索框是否可用，并查看搜索框name值
+        4、打印搜索框这个元素的左上角坐标和它的宽高
+        5、向搜索框输入‘alibaba‘
+        6、判断’阿里巴巴‘是否可见
+        7、如果可见，则打印’搜索成功‘；不可见则打印’搜索失败‘
+        :return:
+        """
+        search_ele = self.driver.find_element_by_id('com.xueqiu.android:id/tv_search')
+        if search_ele.is_enabled():
+            print(search_ele.text)
+            print(search_ele.location)
+            print(search_ele.size)
+
+            search_ele.click()
+            self.driver.find_element_by_id('com.xueqiu.android:id/search_input_text').send_keys('alibaba')
+            ali_element = self.driver.find_element_by_xpath('//*[@resource-id="com.xueqiu.android:id/name" and @text="阿里巴巴"]')
+            if ali_element.is_displayed() == 'true':
+                print('搜索成功')
+            else:
+                print('搜索失败')
+
+
 if __name__ == '__main__':
     pytest.main()
 
